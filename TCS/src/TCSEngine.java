c73b942f542dd93eb7d88aabea41fda7afa19658
Paul <paul@flyleapstudios.com>
2017-11-02 17:54:53 +1100

    changes the engine

--- TCS/src/TCSEngine.java
+++ TCS/src/TCSEngine.java
@@ -3,7 +3,7 @@ public class TCSEngine
 
 	public static void main(String[] args)
 	{
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD - New");
 		
 		TCSWindow TCS = new TCSWindow();
 	}
